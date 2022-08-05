fbea31876f0961410ff0dd54ef89ad453188e8c3
Oleg Danilyuk <danilyuk_oleg@vk.com>
2022-08-05 14:22:27 +0300

    выполнил

--- scripts.js
+++ scripts.js
@@ -4,10 +4,11 @@ let quessTheNumber;
 
 function botNumber() {
   let num = 22;
+  let res = 10;
   function botQuess() {
     quessTheNumber = prompt("Угадай число от 1 до 100");
 
-    if (isNaN(quessTheNumber)) {
+    if (isNaN(quessTheNumber) || quessTheNumber == "") {
       alert("Введите число!");
       botQuess();
     }
@@ -17,13 +18,21 @@ function botNumber() {
     }
 
     if (parseInt(quessTheNumber) === num) {
-      alert("Поздравляю, Вы угадали!!!");
+      confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+      res = 10;
     } else if (parseInt(quessTheNumber) > num) {
-      alert("Загаданное число меньше");
+      res--;
+      alert(`Загаданное число меньше ,осталось попыток ${res}`);
       botQuess();
     } else if (parseInt(quessTheNumber) < num) {
-      alert("Загаданное число больше");
+      res--;
+      alert(`Загаданное число больше, осталось попыток ${res}`);
       botQuess();
+    }
+
+    if (res >= 0) {
+      confirm("Попытки закончились, хотите сыграть еще?");
+      res = 10;
     }
   }
 
